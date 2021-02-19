9c9b753b0045b5c1cbbf300e3602aa897e7f0edd
Daniel Angosto Martinez <Daniel.Angosto.Martinez@es.ey.com>
2021-02-19 12:07:20 +0100

    Product controller example action

---
+++ YuShop/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YuShop.Models;
+
+namespace YuShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products/Random
+        public ActionResult Random()
+        {
+
+            var product = new Product() { Name = "Vela cumpleaños 3" };
+
+            return View(product);
+        }
+    }
+}
---
+++ YuShop/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YuShop.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Type { get; set; }
+
+
+    }
+}
