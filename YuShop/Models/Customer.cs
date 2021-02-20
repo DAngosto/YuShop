ac49a16a7d13f0953dc43022e462ed961e472bdd
Daniel Angosto Martinez <Daniel.Angosto.Martinez@es.ey.com>
2021-02-20 17:58:31 +0100

    Apply anotations to customer entity

--- YuShop/Models/Customer.cs
+++ YuShop/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,8 @@ namespace YuShop.Models
     {
 
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public String Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType membershipType { get; set; }
