751c3bd3ebd48cf57f65a84607704c952cf8c32d
Daniel Angosto Martinez <Daniel.Angosto.Martinez@es.ey.com>
2021-02-20 13:01:26 +0100

    added DB codefirst type

---
+++ YuShop/Models/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace YuShop.Models
+{
+    public class MyDbContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Costumers { get; set; }
+        public MyDbContext()
+        {
+            
+        }
+    }
+}
